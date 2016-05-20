fa0522f7cab5d789731030b163427df2c47a37cb
Yasmine <yasminemohamed2093@gmail.com>
2016-05-21 01:08:41 +0200

    activate and deactivate user in team is done

---
+++ controller/deactivateUser_inTeam.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once './model/User_Team.php';
+include_once './model/User.php';
+
+
+$user_id = $_POST['data']['user_id'];
+$team_id = $_POST['data']['team_id'];
+
+$admin_id = $_POST['data']['admin_id'];
+$admin_password = $_POST['data']['password'];
+
+$admin_obj = new User();
+
+if($admin_obj->checkTeamAdminPassword($admin_id,$admin_password) == 1)
+{
+    $user_obj = new User_Team();
+    $inactive_user = $user_obj->deactivateUser_inTeam($user_id,$team_id);
+
+    if($inactive_user == 1)
+    {
+        echo "User is Deactivated Successfully";
+    }else{
+        echo "Error in Deactivate User";
+    }
+}else{
+    echo "Invalid Password";
+}
+
+
+
