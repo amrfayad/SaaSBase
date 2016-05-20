18a7fc491fd62b0712b351e63e2bece4ca6da54d
DevHanan <hanan.taha1993@gmail.com>
2016-05-20 11:58:04 +0200

    add return team member

--- model/Team.php
+++ model/Team.php
@@ -23,8 +23,12 @@ class Team {
             if (!$conection) {
                 die('Error: in connection Team');
             }
-            $query = "select team_id from teams where users_user_id='".$admin_id."'";
+            $query = "select team_id from teams where users_user_id=$admin_id";
+           // echo $query; exit;
             $result = mysqli_query($conection, $query);
+              while ($row = mysqli_fetch_assoc($result)) {
+                return $row;
+            }
         } catch (Exception $e) {
             echo $e->getMessage();
         }
@@ -36,8 +40,16 @@ class Team {
             if (!$conection) {
                 die('Error: in connection Team');
             }
-            $query = "select * from users u ,users_in_teams t where t.users_user_id='u.user_id and t.teams_team_id= '".$admin_id."'";
+            $query = "select user_name ,user_email,user_profile_info from users u , users_in_teams t where t.users_user_id=u.user_id and t.teams_team_id= $team_id";
+            //echo $query; exit;
             $result = mysqli_query($conection, $query);
+            $a = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                  $a[] = $row;
+                  
+            }
+            return $a;
+             
         } catch (Exception $e) {
             echo $e->getMessage();
         }
