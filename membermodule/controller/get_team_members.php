6ab4fe7c0951088d97f50d037b865676de7afd0a
DevHanan <hanan.taha1993@gmail.com>
2016-05-30 16:52:10 +0200

    Modify get_team_Member to accept admin_email or team_id

--- membermodule/controller/get_team_members.php
+++ membermodule/controller/get_team_members.php
@@ -7,22 +7,39 @@ $user= new User();
 
 //declartion
 $response = array();
-$team_id=$data['team_id'];
 
-if( isset($data['team_id']) && $data['team_id'] != null && 
-	(!filter_var($data['team_id'], FILTER_VALIDATE_INT) === false))
+if( isset($data['team_id']))
 {
+	if($data['team_id'] != null){
+		if((!filter_var($data['team_id'], FILTER_VALIDATE_INT) === false))
+		{
+	$team_id=$data['team_id'];
 	$team_member=$team->getTeamMember($team_id);
 	$response['message'] = 'success';
     $response['status'] = 200;
     $response['data']=$team_member;
     echo json_encode($response);
-
+}
+else{
+$response['message'] = 'failed, Non Vaild Inserted Data';
+$response['status'] = 400;
+echo json_encode($response);
+}
+}
+else
+{
+$response['message'] = 'failed, empty data not applicable';
+$response['status'] = 400;
+echo json_encode($response);
+}
 }
 
-else if (isset($data['email'])&& $data['email'] != null && 
-	(filter_var($data['email'], FILTER_VALIDATE_EMAIL)=== true))
+else if (isset($data['email']))
 {
+	if( $data['email'] != null )
+	{
+		if(!(filter_var($data['email'], FILTER_VALIDATE_EMAIL)=== false))
+		{
 	$email=$data['email'];
 	$admin_id=$user->admin_id($email);
 	$team_id=$team->getTeams($admin_id);
@@ -31,23 +48,21 @@ else if (isset($data['email'])&& $data['email'] != null &&
     $response['status'] = 200;
     $response['data']=$team_member;
     echo json_encode($response);
-
+}
+else{
+$response['message'] = 'failed, Non Vaild Inserted Data';
+$response['status'] = 400;
+echo json_encode($response);
 }
 
+}
 else
-{
-
-if ($data['team_id'] == null)
 {
 $response['message'] = 'failed, empty data not applicable';
 $response['status'] = 400;
 echo json_encode($response);
 }
+}
 
-else if (filter_var( $data['team_id'], FILTER_VALIDATE_INT) === false) {
-$response['message'] = 'failed, Non Vaild Inserted Data';
-$response['status'] = 400;
-echo json_encode($response); 
-}
-}
+
 
--- paymentmodule/controller/Record_Payment.php
+++ paymentmodule/controller/Record_Payment.php
@@ -8,11 +8,12 @@ $team = new Team();
 //declartion
 $response = array();
 $team_id = $data['team_id'];
-$plan_id = $data['subscr_id'];
+$data['plan_id']=2;
+$plan_id = $data['plan_id'];
 
-if (isset($data['team_id']) && isset($data['subscr_id'])) {
-    if ($data['team_id'] != null & $data['subscr_id'] != null) {
-        if ((!filter_var($data['team_id'], FILTER_VALIDATE_INT) === false) && (!filter_var($data['subscr_id'], FILTER_VALIDATE_INT) === false)) {
+if (isset($data['team_id']) && isset($data['plan_id'])) {
+    if ($data['team_id'] != null & $data['plan_id'] != null) {
+        if ((!filter_var($data['team_id'], FILTER_VALIDATE_INT) === false) && (!filter_var($data['plan_id'], FILTER_VALIDATE_INT) === false)) {
             $team_status = $team->asignPlan($team_id,$plan_id);
             $response['message'] = 'success';
             $response['status'] = 200;
