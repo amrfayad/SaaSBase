9c9dff54136c5d20d077be315721b252a89efa9e
ayaemahmoud <ayae.mahmoud@gmail.com>
2016-06-05 14:06:42 +0200

    Modify Get Paymnet Link

--- paymentmodule/controller/record_failed_payment.php
+++ paymentmodule/controller/record_failed_payment.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once '/var/www/html/SaaSBase/models/User.php';
+include_once './models/Team.php';
 //initalize object
 $team = new Team();
 
@@ -16,28 +16,29 @@ if (isset($data['team_id']) && $data['team_id'] != null &&
         $response['message'] = 'success';
         $response['status'] = 200;
 
-        echo "json_encode($response)";
+//        echo json_encode($response);
     }
     else{
         $response['message'] = 'Team Does not Exist';
         $response['status'] = 400;
-        echo json_encode($response);
+//        echo json_encode($response);
     }
 } else {
     if ($data['team_id'] == null) {
         $response['message'] = 'Failed , Empty Data Not Applicable';
         $response['status'] = 400;
-        echo json_encode($response);
+//        echo json_encode($response);
     } else if (filter_var($data['team_id'], FILTER_VALIDATE_INT) === false) {
         $response['message'] = 'Failed, Non Valid Inserted Data';
         $response['status'] = 400;
-        echo json_encode($response);
+//        echo json_encode($response);
     }
 
     else {
         $response['message'] = 'Unexpected Error While getting team Plans';
         $response['status'] = 400;
-        echo json_encode($response);
+//        echo json_encode($response);
     }
 }
 
+echo json_encode($response);
